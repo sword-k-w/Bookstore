27f285a9229ae02762b3abf7fdc8c41b6afde588
sword <sword-k@sjtu.edu.cn>
2024-12-21 12:10:00 +0800

    add time sorting in report employee, and add some titles

--- src/log.cpp
+++ src/log.cpp
@@ -1,6 +1,8 @@
 #include "log.h"
 #include <iostream>
+#include <sstream>
 #include <iomanip>
+#include <algorithm>
 
 void LogSystem::RecordTime(const size_t &time) {
   finance_.WriteInfo(time, 1);
@@ -82,75 +84,110 @@ std::array<char, 400> ToInfo(const std::string &s) {
   return res;
 }
 
+constexpr int kWidthParameter1 = 7;
+constexpr int kWidthParameter2 = 35;
+
 void LogSystem::RecordOperation(const Operation &operation) {
   std::array<char, 400> info;
   std::fill(info.begin(), info.end(), 0);
   Account account;
+  std::stringstream ss;
   switch (operation.operation_type_) {
     case Operation::kLogin : {
-      info = ToInfo(std::to_string(operation.login_operation_.time_) + "\t\t" + ToString(operation.login_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tlogin");
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.login_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.login_operation_.cur_account_.UserID());
+      ss << "login";
+      info = ToInfo(ss.str());
       if (operation.login_operation_.cur_account_.Privilege() >= 3) {
         account = operation.login_operation_.cur_account_;
       }
       break;
     }
     case Operation::kLogout : {
-      info = ToInfo(std::to_string(operation.logout_operation_.time_) + "\t\t" + ToString(operation.logout_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tlogout");
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.logout_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.logout_operation_.cur_account_.UserID());
+      ss << "logout";
+      info = ToInfo(ss.str());
       if (operation.logout_operation_.cur_account_.Privilege() >= 3) {
         account = operation.logout_operation_.cur_account_;
       }
       break;
     }
     case Operation::kRegister : {
-      info = ToInfo(std::to_string(operation.register_operation_.time_) + "\t\t" + ToString(operation.register_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tregister");
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.register_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.register_operation_.cur_account_.UserID());
+      ss << "register";
+      info = ToInfo(ss.str());
       break;
     }
     case Operation::kCreate : {
-      info = ToInfo(std::to_string(operation.create_operation_.time_) + "\t\t" + ToString(operation.create_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tcreate " + ToString(operation.create_operation_.new_account_.UserID()));
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.create_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.create_operation_.cur_account_.UserID());
+      ss << "create " << ToString(operation.create_operation_.new_account_.UserID());
+      info = ToInfo(ss.str());
       account = operation.create_operation_.cur_account_;
       break;
     }
     case Operation::kDelete : {
-      info = ToInfo(std::to_string(operation.delete_operation_.time_) + "\t\t" + ToString(operation.delete_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tdelete " + ToString(operation.delete_operation_.deleted.UserID()));
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.delete_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.delete_operation_.cur_account_.UserID());
+      ss << "delete " << ToString(operation.delete_operation_.deleted.UserID());
+      info = ToInfo(ss.str());
       account = operation.delete_operation_.cur_account_;
       break;
     }
     case Operation::kPassword : {
-      info = ToInfo(std::to_string(operation.password_operation_.time_) + "\t\t" + ToString(operation.password_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tmodify his/her password from " + ToString(operation.password_operation_.cur_account_.Password()) + " to " + ToString(operation.password_operation_.new_password_));
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.password_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.password_operation_.cur_account_.UserID());
+      ss << "modify his/her password from " << ToString(operation.password_operation_.cur_account_.Password()) << " to " << ToString(operation.password_operation_.new_password_);
+      info = ToInfo(ss.str());
       break;
     }
     case Operation::kSearch : {
-      info = ToInfo(std::to_string(operation.search_operation_.time_) + "\t\t" + ToString(operation.search_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tsuccessfully search");
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.search_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.search_operation_.cur_account_.UserID());
+      ss << "successfully search";
+      info = ToInfo(ss.str());
       break;
     }
     case Operation::kModify : {
-      info = ToInfo(std::to_string(operation.modify_operation_.time_) + "\t\t" + ToString(operation.modify_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tmodify the book " + ToString(operation.modify_operation_.book_) + " to " + ToString(operation.modify_operation_.new_book_));
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.modify_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.modify_operation_.cur_account_.UserID());
+      ss << "modify the book " + ToString(operation.modify_operation_.book_) + " to " + ToString(operation.modify_operation_.new_book_);
+      info = ToInfo(ss.str());
       account = operation.modify_operation_.cur_account_;
       break;
     }
     case Operation::kImport : {
-      std::string tt = std::to_string(operation.import_operation_.time_) + "\t\t" + ToString(operation.import_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\timport " + std::to_string(operation.import_operation_.quantity_) + " book with -ISBN= " + ToString(operation.import_operation_.book_.ISBN_) + " and cost " + std::to_string(operation.import_operation_.cost_);
-      info = ToInfo(tt);
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.import_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.import_operation_.cur_account_.UserID());
+      ss << "import " << std::to_string(operation.import_operation_.quantity_) + " book with -ISBN= " + ToString(operation.import_operation_.book_.ISBN_) + " and cost " + std::to_string(operation.import_operation_.cost_);
+      info = ToInfo(ss.str());
       account = operation.import_operation_.cur_account_;
       std::pair<double, double> tmp;
       finance_.Read(tmp, finance_.Length() - 1);
-      tt += " || current income : " + std::to_string(tmp.first) + " || current expenditure : " + std::to_string(tmp.second);
-      auto t = ToInfo(tt);
+      ss << " || current income : " << std::to_string(tmp.first) << " || current expenditure : " << std::to_string(tmp.second);
+      auto t = ToInfo(ss.str());
       finance_report_.Write(t);
       break;
     }
     case Operation::kBuy : {
-      std::string tt = std::to_string(operation.buy_operation_.time_) + "\t\t" + ToString(operation.buy_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\tbuy " + std::to_string(operation.buy_operation_.quantity_) + " book with -ISBN= " + ToString(operation.buy_operation_.book_.ISBN_) + " and cost " + std::to_string(operation.buy_operation_.quantity_ * operation.buy_operation_.book_.price_);
-      info = ToInfo(tt);
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.buy_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.buy_operation_.cur_account_.UserID());
+      ss << "buy " << std::to_string(operation.buy_operation_.quantity_) + " book with -ISBN= " + ToString(operation.buy_operation_.book_.ISBN_) + " and cost " + std::to_string(operation.buy_operation_.quantity_ * operation.buy_operation_.book_.price_);
+      info = ToInfo(ss.str());
       std::pair<double, double> tmp;
       finance_.Read(tmp, finance_.Length() - 1);
-      tt += " || current income : " + std::to_string(tmp.first) + " || current expenditure : " + std::to_string(tmp.second);
-      auto t = ToInfo(tt);
+      ss << " || current income : " << std::to_string(tmp.first) << " || current expenditure : " << std::to_string(tmp.second);
+      auto t = ToInfo(ss.str());
       finance_report_.Write(t);
       break;
     }
     default : {
-      info = ToInfo(std::to_string(operation.exit_operation_.time_) + "\t\t" + ToString(operation.exit_operation_.cur_account_.UserID()) + "\t\t\t\t\t\t\t\t\texit the system");
+      ss << std::left << std::setw(kWidthParameter1) << std::to_string(operation.exit_operation_.time_);
+      ss << std::left << std::setw(kWidthParameter2) << ToString(operation.exit_operation_.cur_account_.UserID());
+      ss << "exit the system";
+      info = ToInfo(ss.str());
     }
   }
   log_.Write(info);
@@ -170,7 +207,8 @@ std::ostream &operator << (std::ostream &os, const std::array<char, 400> &x) {
 
 void LogSystem::ReportLog() {
   std::cout << "The Log of Sword's Bookstore\n";
-  std::cout << "time\tUserID\t\t\t\t\t\t\t\t\toperation\n";
+  std::cout << std::left << std::setw(kWidthParameter1) << "time";
+  std::cout << std::left << std::setw(kWidthParameter2) << "UserID" << "operation\n";
   size_t size = log_.Length();
   for (size_t i = 0; i < size; ++i) {
     std::array<char, 400> tmp;
@@ -180,6 +218,9 @@ void LogSystem::ReportLog() {
 }
 
 void LogSystem::ReportFinance() {
+  std::cout << "The Finance Report of Sword's Bookstore\n";
+  std::cout << std::left << std::setw(kWidthParameter1) << "time";
+  std::cout << std::left << std::setw(kWidthParameter2) << "UserID" << "operation\n";
   size_t size = finance_report_.Length();
   for (size_t i = 0; i < size; ++i) {
     std::array<char, 400> tmp;
@@ -188,9 +229,25 @@ void LogSystem::ReportFinance() {
   }
 }
 
+int ToTime(const std::array<char, 400> &x) {
+  int res = 0;
+  for (size_t i = 0; i < 6; ++i) {
+    if (isdigit(x[i])) {
+      res = res * 10 + x[i] - '0';
+    }
+  }
+  return res;
+}
+
 void LogSystem::ReportEmployee() {
+  std::cout << "The Employee Report of Sword's Bookstore\n";
   auto tmp = employee_report_.FindAllInfo();
+  std::sort(tmp.begin(), tmp.end(), [&] (const std::pair<std::array<char, 30>, std::array<char, 400>> &x, const std::pair<std::array<char, 30>, std::array<char, 400>> &y) {
+    return ToTime(x.second) < ToTime(y.second);
+  });
   std::cout << "root's working log:\n";
+  std::cout << std::left << std::setw(kWidthParameter1) << "time";
+  std::cout << std::left << std::setw(kWidthParameter2) << "UserID" << "operation\n";
   for (auto &x : tmp) {
     if (ToString(x.first) == "root") {
       std::cout << x.second << '\n';
@@ -206,6 +263,8 @@ void LogSystem::ReportEmployee() {
       std::cout << '\n';
       las = cur;
       std::cout << cur << "'s working log:\n";
+      std::cout << std::left << std::setw(kWidthParameter1) << "time";
+      std::cout << std::left << std::setw(kWidthParameter2) << "UserID" << "operation\n";
     }
     std::cout << x.second << '\n';
   }
